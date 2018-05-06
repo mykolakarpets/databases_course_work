8a82443e975b5ee405873d63a4246edaf323b844
Mykola Karpets <kol9karpec@gmail.com>
2018-05-06 10:10:23 +0300

    Task #1 completely done and checked.
    Signed-off-by: Mykola Karpets <kol9karpec@gmail.com>

--- task_1.sql
+++ task_1.sql
@@ -5,8 +5,16 @@ drop procedure if exists get_facilities_by_type;
 delimiter $$
 create procedure get_facilities_by_type(IN type_name varchar(20))
 begin
-	select *
+	select f.id as 'ID',
+				f.name as 'Facility name',
+                ft.name as 'Type name',
+                a.name as 'Attribute name',
+                _af.int_value as 'Integer value',
+                _af.string_value as 'String value'                
 	from facility as f
+    join facility_type as ft 			on f.facility_type_id = ft.id
+    join _attribute_facility as _af 	on f.id = _af.facility_id
+    join attribute as a 						on a.id = _af.attribute_id
 	where f.facility_type_id = get_type_id(type_name);
 end$$
 
@@ -19,7 +27,9 @@ create procedure get_facilities_by_str_param(IN type_name varchar(20),
 			IN attr_name varchar(20),
             IN attr_value varchar(20))
 begin
-	select *
+	select f.name as 'Facility name',
+				attr.name as 'Attribute name',
+                _af.string_value as 'Value'
 	from facility as f
 	join _attribute_facility as _af
 	join attribute as attr
@@ -43,11 +53,9 @@ create procedure get_facilities_by_int_param(IN type_name varchar(20),
             IN low_bound int,
             IN top_bound int)
 begin
-	select *
-	from facility as f
-	where f.facility_type_id = get_type_id(type_name);
-
-	select *
+	select f.name as 'Facility name',
+				attr.name as 'Attribute name',
+                _af.int_value as 'Value'
 	from facility as f
 	join _attribute_facility as _af
 	join attribute as attr
@@ -61,3 +69,10 @@ begin
 			and _af.int_value >= low_bound
             and _af.int_value <= top_bound;
 end$$
+
+#--------------------------------------------------------------------------
+delimiter ;
+
+call get_facilities_by_type("facility_type_1");
+call get_facilities_by_str_param("facility_type_1", "str_attribute_1", "facility_str_val_1");
+call get_facilities_by_int_param("facility_type_1", "int_attribute_3", 0, 10);
